bcf8a98b13f51efae9406b770ac34f3239f69de9
Touma-M08 <touma_064810@yahoo.co.jp>
2022-10-20 11:02:20 +0900

    使用ファイルの作成

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+    return (
+        <div>
+            <section>
+                <h1>プロフィール</h1>
+                
+                <h2>森野刀磨 -Morino Toma-</h2>
+                
+                {/*生年月日とか学歴、スキル等*/}
+            </section>
+            
+            <section>
+                <h1>制作物</h1>
+            </section>
+        </div>
+    );
+}
+
+export default Home;
