be0c2742c88e444f21f369fff1eb54bda548cd90
Touma-M08 <touma_064810@yahoo.co.jp>
2022-10-21 00:20:36 +0900

    プロフィール画像の登録機能実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\User;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(User $user)
     {
-        return Inertia::render("Home");
+        return Inertia::render("Home", ["user" => $user->first()]);
     }
 }
