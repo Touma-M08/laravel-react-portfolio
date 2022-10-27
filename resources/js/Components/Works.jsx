7472dec15d482cb65ce1a84df96ea0990b873c88
Touma-M08 <touma_064810@yahoo.co.jp>
2022-10-27 11:03:14 +0900

    トップページのcss追加

--- resources/js/Components/Works.jsx
+++ resources/js/Components/Works.jsx
@@ -1,35 +1,38 @@
-import Work from "../../image/gathery.jpg";
-import "./Works.css";
+import Work1 from "../../image/gathery.jpg";
 
 import { Link } from "@inertiajs/inertia-react";
 
 const Works = () => {
     const works = [
         {
-            image: Work,
+            image: Work1,
             title: "Gathery"
+        },
+        {
+            image: Work1,
+            title: "portfolio"
         },
     ];
     
     return (
-        <div>
-            <h1>制作物</h1>
-            
-            <ul>
-                {works.map((work, index) => (
-                    <div>
-                        <div className="work" key={index}>
-                            <Link href={`/works/${index}`}>
+        <div className="works">
+            <div class="wrap">
+                <h1 className="content-ttl __works" id="works">製作実績</h1>
+                
+                <ul>
+                    {works.map((work, index) => (
+                        <li className="work" key={index}>
+                            <Link className="work-link" href={`/works/${index}`}>
                                 <div className="work-img-box">
                                     <img className="work-img" src={work.image} />
                                 </div>
-                                <h3>{work.title}</h3>
+                                <h3 className="work-ttl">{work.title}</h3>
                             </Link>
-                        </div>
-                    </div>
-                ))}
-            </ul>
-        </div>  
+                        </li>
+                    ))}
+                </ul>
+            </div>  
+        </div>
     );
 }
 
